d27c2c72526d74f320222ab63fc63e350766ceca
Ryan <ryanjerickson@gmail.com>
2023-07-28 16:41:28 -0500

    Server Seeds

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -1,24 +1,23 @@
-from app.models import db, User, environment, SCHEMA
+from app.models import db, Server, environment, SCHEMA
 from sqlalchemy.sql import text
 
 
 # Adds a demo user, you can add other users here if you want
-def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
-    timmy = User(
-        username='timmy', email='timmy@aa.io', password='password')
+def seed_servers():
+    north = Server(
+        name='North', created_by=1, is_public=True, description='Server for the North Side')
+    south = Server(
+        name='South', created_by=2, is_public=True, description='Server for the South Side')
+    east = Server(
+        name='East', created_by=3, is_public=True, description='Server for the East Side')
+    west = Server(
+        name='West', created_by=4, is_public=True, description='Server for the West Side')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
-    db.session.add(timmy)
+    db.session.add(north)
+    db.session.add(south)
+    db.session.add(east)
+    db.session.add(west)
     db.session.commit()
-
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
 # have a built in function to do this. With postgres in production TRUNCATE
@@ -26,7 +25,7 @@ def seed_users():
 # incrementing primary key, CASCADE deletes any dependent entities.  With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
-def undo_users():
+def undo_servers():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
