f004a87e9f4200ade3e5896776225fdbfaf1c496
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-29 12:06:27 -0400

    placeholders in place

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -4,23 +4,28 @@ import './landingPage.css'
 const LandingPage = () => {
 
     return (
-        <div id='signUpContainer'>
-            <div id='signUpInfoContainer'>
-                <h1 id='signUpInfoHeader'>Made for people. <span>Built for productivity</span></h1>
-                <p>Connect the right people, find anything you need and automate the rest. That’s work in Slack,
-                    your productivity platform.
-                </p>
-                <div id='signUpButtonContainer'>
-                    <button>SIGN UP WITH EMAIL</button>
-                    <button> <img id='signUpGoogleLogo' src='https://static.vecteezy.com/system/resources/previews/013/948/549/original/google-logo-on-transparent-white-background-free-vector.jpg'></img>
-                        SIGN UP WITH GOOGLE
-                    </button>
+        <div id=''>
+            <div id='signUpContainer'>
+                <div id='signUpInfoContainer'>
+                    <h1 id='signUpInfoHeader'>Made for people. <span>Built for productivity.</span></h1>
+                    <p className='signUpInfoDesc'>Connect the right people, find anything you need and automate the rest. That’s work in Slacker,
+                        your productivity platform.
+                    </p>
+                    <div id='signUpButtonContainer'>
+                        <button>SIGN UP WITH EMAIL</button>
+                        <button id='signUpGoogle'> <img id='signUpGoogleLogo' src='https://static.vecteezy.com/system/resources/previews/013/948/549/original/google-logo-on-transparent-white-background-free-vector.jpg'></img>
+                            SIGN UP WITH GOOGLE
+                        </button>
+                    </div>
+                    <p className='signUpInfoDesc'>
+                        Slacker is free to try for as long as you'd like
+                    </p>
                 </div>
-            </div>
 
-            <div id='signUpVideoContainer'>
-                <div>
-                    <h1>Pretend there is a video here</h1>
+                <div id='signUpVideoContainer'>
+                    <div>
+                        <h1>Pretend there is a video here</h1>
+                    </div>
                 </div>
             </div>
         </div>
