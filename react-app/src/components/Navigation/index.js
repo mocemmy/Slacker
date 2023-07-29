a5f76d707087c95f9572c56d6027673fd558d185
Emily Morgan <mocemmy@aol.com>
2023-07-29 15:29:05 -0400

    nav changes

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul className='nav-container'>
-			<li className='navlinks'>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li className='navlinks'>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+  return (
+      <div className="nav-content-container">
+    <ul className="nav-container">
+        <li className="navlinks">
+          <NavLink className="home-link" exact to="/">
+            Slacker
+          </NavLink>
+        </li>
+        {isLoaded && (
+          <li className="navlinks">
+            <ProfileButton user={sessionUser} />
+          </li>
+        )}
+    </ul>
+      </div>
+  );
 }
 
 export default Navigation;
