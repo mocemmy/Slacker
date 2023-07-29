3378dfabf924c04a79970b72888d6e26c5a3e9b9
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-29 13:06:30 -0400

    changed css file name and completed signup block on landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,39 +1,41 @@
 import React, { useState } from 'react'
-import './landingPage.css'
+import './signUpBlock.css'
 
 const LandingPage = () => {
 
     return (
-        <section id='signUpInfoSection'>
-            <div id='signUpContainer'>
-                <div id='signUpInfoContainer'>
-                    <h1 id='signUpInfoHeader'>Made for people. <span>Built for productivity.</span></h1>
-                    <p className='signUpInfoDesc'>Connect the right people, find anything you need and automate the rest. That’s work in Slacker,
-                        your productivity platform.
-                    </p>
-                    <div id='signUpButtonContainer'>
-                        <a id='signUpButton'>SIGN UP WITH EMAIL</a>
-                        <a id='signUpGoogle'>
-                            <div id='signUpGoogleLogoContainer'>
-                                <img id='signUpGoogleLogo' src='https://static.vecteezy.com/system/resources/previews/013/948/549/original/google-logo-on-transparent-white-background-free-vector.jpg'></img>
-                            </div>
-                            <div>
-                                SIGN UP WITH GOOGLE
-                            </div>
-                        </a>
+        <>
+            <section id='signUpInfoSection'>
+                <div id='signUpContainer'>
+                    <div id='signUpInfoContainer'>
+                        <h1 id='signUpInfoHeader'>Made for people. <span>Built for productivity.</span></h1>
+                        <p className='signUpInfoDesc'>Connect the right people, find anything you need and automate the rest. That’s work in Slacker,
+                            your productivity platform.
+                        </p>
+                        <div id='signUpButtonContainer'>
+                            <a id='signUpButton'>SIGN UP WITH EMAIL</a>
+                            <a id='signUpGoogle'>
+                                <div id='signUpGoogleLogoContainer'>
+                                    <img id='signUpGoogleLogo' src='https://static.vecteezy.com/system/resources/previews/013/948/549/original/google-logo-on-transparent-white-background-free-vector.jpg'></img>
+                                </div>
+                                <div>
+                                    SIGN UP WITH GOOGLE
+                                </div>
+                            </a>
+                        </div>
+                        <p className='signUpInfoDesc'>
+                            Slacker is free to try for as long as you'd like
+                        </p>
                     </div>
-                    <p className='signUpInfoDesc'>
-                        Slacker is free to try for as long as you'd like
-                    </p>
-                </div>
 
-                <div id='signUpVideoContainer'>
-                    <div>
-                        <h1>Pretend there is a video here</h1>
+                    <div id='signUpVideoContainer'>
+                        <div>
+                            <h1>Pretend there is a video here</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
