f8fc7703f60abed71a22ccc639ec7d180b1ab376
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-30 12:58:25 -0400

    merged branches

--- app/api/test_routes.py
+++ app/api/test_routes.py
@@ -1,11 +1,26 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
-from app.models import User, Server, Channel, Message
+from app.models import db, User, Server, Channel, Message
 
 test_routes = Blueprint('test', __name__)
 
 @test_routes.route('/')
 @login_required
 def test_index():
-    print(current_user)
-    return {"message": "This is a test route"}
+    return {"current_user": current_user.to_dict()}
+
+@test_routes.route('/servers-and-channels')
+@login_required
+def test_get_servers_and_channels():
+    print('---------------test route----------------')
+    servers = current_user.servers
+    channels = current_user.channels
+    print('---------------end route----------------')
+    return {"servers": [server.to_dict() for server in servers],
+            "channels": [channel.to_dict() for channel in channels]}
+
+@test_routes.route('/channel/<int:channel_id>/messages')
+@login_required
+def test_get_messages_in_channel(channel_id):
+  messages = Message.query.filter(Message.channel_id == channel_id).order_by(Message.created_at)
+  return {"messages": [message.to_dict() for message in messages]}
