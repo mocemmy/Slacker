185af2ca5bdaef736a8e651e9c1a8f0799beae93
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-30 12:55:05 -0400

    made a test compenent for websocket

--- react-app/src/components/WebsocketTesting/index.js
+++ react-app/src/components/WebsocketTesting/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
+
+
 
 const SocketTesting = () => {
+    const [socket, setSocket] = useState('')
+
+    useEffect(() => {
+        const newSocket = io.connect('https://localhost:5000')
+
+        setSocket(newSocket)
+    }, [setSocket])
 
     return (
         <>
             <h1>Testing</h1>
+            <ul>
+                <li></li>
+            </ul>
+
+            <form>
+                <textarea placeholder='Message'></textarea>
+                <button type='submit'></button>
+            </form>
         </>
     )
 }
