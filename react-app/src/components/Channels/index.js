47955c9f660af865e6ae3180e5316b74faf3f86c
Ryan <ryanjerickson@gmail.com>
2023-07-31 18:42:06 -0500

    Basic Socket

--- react-app/src/components/Channels/index.js
+++ react-app/src/components/Channels/index.js
@@ -1,29 +1,42 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux";
 import Loading from '../Loading';
 import { thunkGetAllMessages } from "../../store/messages";
+import Messages from "../Messages";
 
 function Channels() {
     const channels = useSelector(state => state.channels.channelList)
     const dispatch = useDispatch()
+    const [defaultChannel, setDefaultChannel] = useState()
 
     const changeChannel = (e, currChannel) => {
         dispatch(thunkGetAllMessages(currChannel))
+        setDefaultChannel(currChannel)
     }
 
-    if(!channels) return <Loading />
+    useEffect(() => {
+        if (channels && channels[0]) {
+            setDefaultChannel(channels[0].id)
+            dispatch(thunkGetAllMessages(defaultChannel))
+        }
+    }, [channels, dispatch])
 
-    if(channels[0]) {
-        const defaultChannel = channels[0].id
-        dispatch(thunkGetAllMessages(defaultChannel))
-    }
+    if (!channels) return <Loading />
+
 
     return (
-        <ul>
-            {channels.map(currChannel => (
-                <li key={currChannel.id} onClick={(e) => changeChannel(e, currChannel.id)}>{currChannel.name}</li>
-            ))}
-        </ul>
+        <>
+            <div className="channel-container">
+            <ul>
+                {channels.map(currChannel => (
+                    <li key={currChannel.id} onClick={(e) => changeChannel(e, currChannel.id)}>{currChannel.name}</li>
+                ))}
+            </ul>
+            </div>
+            <div className="message-container">
+                <Messages channel={defaultChannel} />
+            </div>
+        </>
     )
 }
 
--- react-app/src/components/Messages/index.js
+++ react-app/src/components/Messages/index.js
@@ -1,19 +1,77 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux";
 import Loading from '../Loading'
+import { io } from 'socket.io-client'
 
 
-function Messages() {
+
+function Messages({ }) {
     const messages = useSelector(state => state.messages.messageList)
-    console.error('messages', messages)
-    if(!messages) return <Loading />
+
+    const [socketInstance, setSocketInstance] = useState(null)
+    const [message, setMessage] = useState("");
+    const [messageArr, setMessageArr] = useState([])
+    const [isSending, setIsSending] = useState('');
+    const user = useSelector(state => state.session.user);
+
+
+    const handleText = (e) => {
+        const inputMessage = e.target.value;
+        setMessage(inputMessage);
+    };
+
+    const handleSubmit = () => {
+        socketInstance.emit("my_message", message);
+        setMessage("");
+    };
+
+    useEffect(() => {
+        if (process.env.FLASK_ENV !== 'production') {
+            const socket = io('localhost:5000')
+            setSocketInstance(socket)
+        } else {
+            const socket = io('https://slacker-chat-collab.onrender.com')
+            setSocketInstance(socket)
+        }
+    }, []);
+
+
+    useEffect(() => {
+        if (socketInstance) {
+            socketInstance.on('my_message', (data) => {
+                console.log("Received from socket: ", data)
+                setMessageArr(prevArr => [...prevArr, data])
+            });
+
+            return () => {
+                socketInstance.off("my_message", () => {
+                    console.log("off message listener");
+                });
+            };
+        }
+    }, [socketInstance])
+
+    useEffect(() => {
+        console.log(messageArr)
+    }, [messageArr])
+
+
+
+    if (!messages) return <Loading />
 
     return (
-        <ul>
-            {messages.map(message => (
-                <li key={message.id}>{message.message_body}</li>
-            ))}
-        </ul>
+        <div>
+            <ul>
+                {messages.map(message => (
+                    <li key={message.id}>{message.message_body}</li>
+                ))}
+                {messageArr.map(message => (
+                    <li key={message.id}>{message}</li>
+                ))}
+            </ul>
+            <input type='text' value={message} onChange={(handleText)}></input>
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
     )
 }
 
