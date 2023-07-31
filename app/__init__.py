efe51b283a85c3b486e4ddad0798b812cb09adb2
Ryan <ryanjerickson@gmail.com>
2023-07-31 17:36:58 -0500

    import

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from app.models import db, User
+from flask_socketio import SocketIO, send, emit
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.server_routes import server_routes
