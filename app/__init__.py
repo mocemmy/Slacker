9d081aa7bf0603bf379ad3111b7f6cd91bf98c28
Ryan <ryanjerickson@gmail.com>
2023-07-31 17:38:13 -0500

    origins

--- app/__init__.py
+++ app/__init__.py
@@ -33,6 +33,24 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(server_routes, url_prefix='/api/servers')
 app.register_blueprint(channel_routes, url_prefix='/api/channels')
+
+origins = ""
+
+if os.environ.get("FLASK_ENV") == "production":
+    origins = [
+        "https://slacker-chat-collab.onrender.com"
+    ]
+else:
+    origins = "*"
+
+socketio = SocketIO(app, cors_allowed_origins=origins)
+
+
+@socketio.on('my_message')
+def handle_message(data):
+    emit('my_message', data, broadcast=True)
+
+
 db.init_app(app)
 Migrate(app, db)
 
