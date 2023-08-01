4ea5a468e89bae5026c27ee6f06520661301fa3c
Ryan <ryanjerickson@gmail.com>
2023-07-31 23:28:29 -0500

    Seeder Data

--- app/seeds/channels.py
+++ app/seeds/channels.py
@@ -10,46 +10,67 @@ def seed_channels():
         description = 'This is a general channel for all Corporate staff'
     )
     channel2 = Channel(
-        name = 'all-staff',
+        name = 'all-staff-Scranton',
         server_id = 2,
         created_by = 3,
         is_public = True,
         description = 'This is a general channel for all Scranton staff'
     )
     channel3 = Channel(
-        name = 'all-staff',
+        name = 'all-staff-Stamford',
         server_id = 3,
         created_by = 8,
         is_public = True,
         description = 'This is a general channel for all Stamford staff'
     )
     channel4 = Channel(
-        name = 'all-staff',
+        name = 'all-staff-Utica',
         server_id = 4,
         created_by = 13,
         is_public = True,
         description = 'This is a general channel for all Utica staff'
     )
     channel5 = Channel(
-        name='accounting',
+        name='accounting-Scranton',
         server_id = 2,
         created_by = 3,
         is_public = True,
         description = 'This is the accounting channel for Scranton'
     )
     channel6 = Channel(
-        name = 'sales',
+        name = 'sales-Scranton',
         server_id = 2,
         created_by = 3,
         is_public = True,
         description = 'This is the sales channel for Scranton'
     )
     channel7 = Channel(
-        name = 'security',
+        name = 'security-Scranton',
         server_id = 2,
         created_by = 3,
         is_public = True,
         description = 'This is the security channel for Scranton'
+    )
+    channel8 = Channel(
+        name = 'general-Corpo',
+        server_id = 1,
+        created_by = 1,
+        is_public = True,
+        description = 'This is the general channel for Corporate'
+    )
+    channel9 = Channel(
+        name = 'sales-Utica',
+        server_id = 4,
+        created_by = 13,
+        is_public = True,
+        description = 'This is the sales channel for Utica'
+    )
+    channel10 = Channel(
+        name = 'service-Stamford',
+        server_id = 3,
+        created_by = 8,
+        is_public = True,
+        description = 'This is the customer service channel for Stamford'
     )
 
     db.session.add(channel1)
@@ -59,6 +80,9 @@ def seed_channels():
     db.session.add(channel5)
     db.session.add(channel6)
     db.session.add(channel7)
+    db.session.add(channel8)
+    db.session.add(channel9)
+    db.session.add(channel10)
     db.session.commit()
 
 def undo_channels():
--- app/seeds/messages.py
+++ app/seeds/messages.py
@@ -13,8 +13,11 @@ def seed_messages():
     message3 = Message(
         message_body="You smoke in your office and go to Scranton far too often, you're fired!!", channel_id=1, sent_by=1
     )
+    message24 = Message(
+        message_body="If it's because of my enhancement I'll see you in court!", channel_id=1, sent_by=1
+    )
     message4 = Message(
-        message_body="Goood morning Scranton!", channel_id=2, sent_by=3
+        message_body="Goood morning Viet-Scranton!", channel_id=2, sent_by=3
     )
     message5 = Message(
         message_body="Good morning, Michael! I know you slept well, I saw you.", channel_id=2, sent_by=4
@@ -23,13 +26,13 @@ def seed_messages():
         message_body="...", channel_id=2, sent_by=5
     )
     message7 = Message(
-        message_body="Dwight is creepy", channel_id=2, sent_by=7
+        message_body="Ok Dwight", channel_id=2, sent_by=7
     )
     message8 = Message(
         message_body="Hey everyone, let's listen to Meredith because she's so innocent.", channel_id=2, sent_by=9
     )
     message9 = Message(
-        message_body="This is a work channel, please be respectful.", channel_id=2, sent_by=1
+        message_body="This is a work channel people.", channel_id=2, sent_by=1
     )
     message10 = Message(
         message_body="Hi David, did you get a chance to look over my mockups yet?", channel_id=2, sent_by=6
@@ -40,11 +43,20 @@ def seed_messages():
     message12 = Message(
         message_body="Rit-Dit-Do-Do-Do you have to ask?!", channel_id=3, sent_by=12
     )
+    message25 = Message(
+        message_body="Don't go sniper in Carrington again, Andy.", channel_id=3, sent_by=11
+    )
     message13 = Message(
         message_body="Free for dinner tonight?", channel_id=4, sent_by=13
     )
     message14 = Message(
         message_body="Always!", channel_id=4, sent_by=14
+    )
+    message26 = Message(
+        message_body="Pick you up at 8?", channel_id=4, sent_by=13
+    )
+    message27 = Message(
+        message_body="Great!", channel_id=4, sent_by=14
     )
     message15 = Message(
         message_body="Oscar, I need another advance on my paycheck.", channel_id=5, sent_by=3
@@ -56,19 +68,61 @@ def seed_messages():
         message_body="Monkey problems?", channel_id=5, sent_by=9
     )
     message18 = Message(
-        message_body="Attention Sales Staff: The new website will be live today, good luck!", channel_id=6, sent_by=3
+        message_body="I know you saw that correctly.", channel_id=5, sent_by=8
     )
     message19 = Message(
-        message_body="Do you really think a computer can beat me?", channel_id=6, sent_by=4
+        message_body="Attention Sales Staff: The new website will be live today, good luck!", channel_id=6, sent_by=3
     )
     message20 = Message(
-        message_body="It will.", channel_id=6, sent_by=5
+        message_body="Do you really think a computer can beat me?", channel_id=6, sent_by=4
     )
     message21 = Message(
-        message_body="911", channel_id=7, sent_by=3
+        message_body="It will, Dwight.", channel_id=6, sent_by=5
+    )
+    message28 = Message(
+        message_body="Andy, get the counter.", channel_id=6, sent_by=4
+    )
+    message29 = Message(
+        message_body="Right-O boss-O!", channel_id=6, sent_by=12
     )
     message22 = Message(
-        message_body="Next time I hustle up the stairs for no reason you're paying the price.", channel_id=7, sent_by=10
+        message_body="911", channel_id=7, sent_by=3
+    )
+    message23 = Message(
+        message_body="You know I hustled up the stairs....", channel_id=7, sent_by=10
+    )
+    message29 = Message(
+        message_body="You could use it.", channel_id=7, sent_by=3
+    )
+    message30 = Message(
+        message_body="Advertising team meeting in 10 minutes", channel_id=8, sent_by=2
+    )
+    message31 = Message(
+        message_body="Make it 15, in a call.", channel_id=8, sent_by=1
+    )
+    message32 = Message(
+        message_body="Ok, David.", channel_id=8, sent_by=2
+    )
+    message33 = Message(
+        message_body="Scranton is trying to compete with us, lol", channel_id=9, sent_by=14
+    )
+    message34 = Message(
+        message_body="Be nice...", channel_id=9, sent_by=13
+    )
+    message35 = Message(
+        message_body="Scranton is trying very nicely to compete with us...", channel_id=9, sent_by=14
+    )
+    message36 = Message(
+        message_body=":/", channel_id=9, sent_by=13
+    )
+    message37 = Message(
+        message_body="What's up with my report scores?", channel_id=10, sent_by=12
+    )
+    message38 = Message(
+        message_body="Maybe you should try selling something, Andy.", channel_id=10, sent_by=11
+    )
+    message39 = Message(
+        message_body="Maybe you should try selling...your butt", channel_id=10, sent_by=12
     )
 
     db.session.add(message1)
@@ -93,6 +147,23 @@ def seed_messages():
     db.session.add(message20)
     db.session.add(message21)
     db.session.add(message22)
+    db.session.add(message23)
+    db.session.add(message24)
+    db.session.add(message25)
+    db.session.add(message26)
+    db.session.add(message27)
+    db.session.add(message28)
+    db.session.add(message29)
+    db.session.add(message30)
+    db.session.add(message31)
+    db.session.add(message32)
+    db.session.add(message33)
+    db.session.add(message34)
+    db.session.add(message35)
+    db.session.add(message36)
+    db.session.add(message37)
+    db.session.add(message38)
+    db.session.add(message39)
     db.session.commit()
 
 def undo_messages():
