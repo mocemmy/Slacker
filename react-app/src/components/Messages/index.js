9800873a447302b92488649b8dca2a4ae5e1d1a3
Ryan <ryanjerickson@gmail.com>
2023-08-01 16:20:12 -0500

    Working Rooms

--- react-app/src/components/Messages/index.js
+++ react-app/src/components/Messages/index.js
@@ -1,70 +1,80 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import Loading from '../Loading'
-import { io } from 'socket.io-client'
+import Loading from '../Loading';
+import { io } from 'socket.io-client';
 
+function Messages({ channel }) {
+  const messages = useSelector(state => state.messages.messageList);
 
+  const [socketInstance, setSocketInstance] = useState(null);
+  const [message, setMessage] = useState("");
+  const [messageArr, setMessageArr] = useState([]);
 
-function Messages() {
-    const messages = useSelector(state => state.messages.messageList)
+  const handleText = (e) => {
+    const inputMessage = e.target.value;
+    setMessage(inputMessage);
+  };
 
-    const [socketInstance, setSocketInstance] = useState(null)
-    const [message, setMessage] = useState("");
-    const [messageArr, setMessageArr] = useState([])
+  const handleSubmit = () => {
+    if (!message || !channel) return;
 
-    const handleText = (e) => {
-        const inputMessage = e.target.value;
-        setMessage(inputMessage);
+    socketInstance.emit("my_message", { channel: channel.toString(), message: message });
+
+    setMessage("");
+  };
+
+  useEffect(() => {
+    // Establish socket connection when the component mounts
+    const socket = process.env.NODE_ENV !== 'production' ?
+      io('http://localhost:5000') :
+      io('https://slacker-chat-collab.onrender.com');
+
+    setSocketInstance(socket);
+
+    // Join the specific room when the component mounts
+    if (socket && channel) {
+      socket.emit("join", { room: channel.toString() });
+    }
+
+    return () => {
+      // Clean up the socket connection and leave the room when the component unmounts
+      if (socketInstance && channel) {
+        socket.emit("leave", { room: channel.toString() });
+        socket.disconnect();
+      }
     };
+  }, [channel]);
 
-    const handleSubmit = () => {
-        socketInstance.emit("my_message", message);
-        setMessage("");
-    };
+  useEffect(() => {
+    if (socketInstance) {
+      socketInstance.on('my_message', (data) => {
+        console.log("Received from socket: ", data)
+        const receivedMessage = data.message;
+        setMessageArr(prevArr => ([...prevArr, [channel, receivedMessage]]));
 
-    useEffect(() => {
-        if (process.env.NODE_ENV !== 'production') {
-            const socket = io('localhost:5000')
-            setSocketInstance(socket)
-        } else {
-            const socket = io('https://slacker-chat-collab.onrender.com')
-            setSocketInstance(socket)
-        }
-    }, []);
+      });
+    }
+  }, [socketInstance]);
 
+  if (!messages) return <Loading />;
 
-    useEffect(() => {
-        if (socketInstance) {
-            socketInstance.on('my_message', (data) => {
-                console.log("Received from socket: ", data)
-                setMessageArr(prevArr => [...prevArr, data])
-            });
-
-            return () => {
-                socketInstance.off("my_message", () => {
-                    console.log("off message listener");
-                });
-            };
-        }
-    }, [socketInstance])
+  console.log('MESSAGEARRAY', messageArr)
 
 
-    if (!messages) return <Loading />
-
-    return (
-        <div>
-            <ul>
-                {messages.map(message => (
-                    <li key={message.id}>{message.message_body}</li>
-                ))}
-                {messageArr.map((message, index) => (
-                    <li key={index}>{message}</li>
-                ))}
-            </ul>
-            <input type='text' value={message} onChange={(handleText)}></input>
-            <button onClick={handleSubmit}>Submit</button>
-        </div>
-    )
+  return (
+    <div>
+      <ul>
+        {messages.map(message => (
+          <li key={message.id}>{message.message_body}</li>
+        ))}
+        {messageArr.map((message, index) => (
+          message[0] === channel ? <li key={index}>{message[1]}</li> : null
+        ))}
+      </ul>
+      <input type='text' value={message} onChange={handleText} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
 
 export default Messages;
