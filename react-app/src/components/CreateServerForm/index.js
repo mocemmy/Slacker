ddaa8343e139cef95c0d956caf876846cdfd885e
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-02 14:12:35 -0400

    create new server form in place but not functional

--- react-app/src/components/CreateServerForm/index.js
+++ react-app/src/components/CreateServerForm/index.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const CreateServerForm = () => {
-    // const [serverName, setServerName] = useState("")
+    const [serverName, setServerName] = useState('')
+    const [pfpURL, setPfpURL] = useState('')
+    const [description, setDescription] = useState('')
+    const [isPublic, setIsPublic] = useState(true)
+
+    useEffect(() => {
+        console.log(isPublic)
+    }, [isPublic])
 
     return (
         <form>
@@ -11,9 +18,31 @@ const CreateServerForm = () => {
             <input
                 placeholder='Server name'
                 type='text'
-            // value={serverName}
-            // onChange={(e) => setServerName(e.target.value)}
+                value={serverName}
+                onChange={(e) => setServerName(e.target.value)}
             />
+
+            <label for='server-pic'>Server Picture</label>
+            <input
+                placeholder='Server pic Example: https://funny-pic.png'
+                value={pfpURL}
+                onChange={(e) => setPfpURL(e.target.value)}
+            />
+
+            <label for='description'></label>
+            <input
+                placeholder='Server description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+
+            <div>
+                <button type='button' value={isPublic} onClick={() => setIsPublic(true)}>Public</button>
+                <button type='button' value={isPublic} onClick={() => setIsPublic(false)}>Private</button>
+            </div>
+
+            <button type='submit'>Submit</button>
+
         </form>
     )
 }
