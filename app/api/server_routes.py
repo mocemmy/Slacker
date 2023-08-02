951750fa68454db91b318382e63d07a26a5d5d5c
James Yu Cao <caoyu19920414@gmail.com>
2023-08-02 12:56:38 -0400

    update server partially done

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required, current_user
-from app.models import Server
+from app.models import db, Server
 
 server_routes = Blueprint('servers', __name__)
 
@@ -14,6 +14,9 @@ def servers():
 
     return {"servers": [server.to_dict() for server in servers]}
 
+# Update Server PUT /api/servers/:serverId
+
+
 
 @server_routes.route('/<int:id>/channels')
 @login_required
@@ -24,3 +27,28 @@ def channels(id):
     server = Server.query.get(id)
 
     return {"channels": [channel.to_dict() for channel in server.channels]}
+
+
+@server_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def updateServerById(id):
+    print(dir(current_user))
+    owned_servers = current_user.owned_servers
+    server_id_list = [server.id for server in owned_servers]
+    print(server_id_list)
+    if id in server_id_list:
+        data = request.json
+        # update
+        server = Server.query.get(id)
+        server.name = data["name"]
+        #server.created_by = data["created_by"]
+        server.is_public = data["is_public"]
+        server.profile_pic = data["profile_pic"]
+        server.description = data["description"]
+        #db.session.add(server)
+        db.session.commit()
+        return server.to_dict(), 200
+    else:
+        # error, not authorized
+        pass
+    return {"message":"Update Done"}, 200
