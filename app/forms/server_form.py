bc2f1bf68e9412b7d40fdcc494ecaf87cfe9834a
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-02 16:15:52 -0400

    store in place working on backend

--- app/forms/server_form.py
+++ app/forms/server_form.py
@@ -2,3 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import User
+
+class ServerForm(FlaskForm)
+    name = StringField('Name', DataRequired())
