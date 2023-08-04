360a24411207088ad5e35719e37bd63c9de1934b
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-04 18:02:40 -0400

    finished validation on create/update a server

--- react-app/src/components/ServerForm/index.js
+++ react-app/src/components/ServerForm/index.js
@@ -13,7 +13,7 @@ const ServerForm = ({ server, formType }) => {
     const [errors, setErrors] = useState({});
     const { closeModal } = useModal();
     let title;
-    if (formType === 'create'){
+    if (formType === 'create') {
         title = "Create a Workspace"
     } else {
         title = "Update Workspace"
@@ -39,7 +39,34 @@ const ServerForm = ({ server, formType }) => {
             description: description,
         };
 
+
+        const validateData = () => {
+            const errorObj = {};
+
+            if (serverName.length === 0) errorObj.name = 'This field is required.'
+            if (serverName.length > 30) errorObj.name = 'Must be shorter than 30 characters.'
+            if (serverName.length <= 30) delete Object.name
+
+            if (pfpURL) {
+                const splitImg = pfpURL.split('.')
+                if (!(splitImg[splitImg.length - 1] === 'jpg' || splitImg[splitImg.length - 1] === 'png' || splitImg[splitImg.length - 1] === 'jpeg')) {
+                    errorObj.serverPic = 'Must end in .png, .jpg or .jpeg.';
+                }
+            }
+
+            if (description.length === 0) errorObj.description = 'This field is required.'
+            if (description.length > 255) errorObj.description = 'Must be shorter than 255 characters'
+            if (description.length <= 255) delete Object.description
+
+            if (Object.keys(errorObj).length > 0) return errorObj
+            else return false
+        }
+
         if (formType === "create") {
+            const newErrors = validateData();
+
+            if (newErrors) return setErrors(newErrors)
+
             const response = await dispatch(thunkCreateNewServer(newServer));
             if (response.errors) {
                 console.log(response.errors)
@@ -51,6 +78,10 @@ const ServerForm = ({ server, formType }) => {
             return
         }
         if (formType === "update") {
+            const newErrors = validateData();
+
+            if (newErrors) return setErrors(newErrors)
+
             const response = await dispatch(thunkUpdateServerById(newServer, server.id));
             if (response.errors) {
                 console.log(response.errors)
@@ -64,66 +95,72 @@ const ServerForm = ({ server, formType }) => {
     }
 
     return (
-        <form className='form-container'>
-            <h1 className="modal-title">{title}</h1>
-            <label htmlFor='name' className='form-label'>Name</label>
-            {errors.name && <p className='new-server-form-error'> Name Error: {errors.name}</p>}
-            <input
-                name='name'
-                id='new-server-form-name'
-                placeholder='workspace name'
-                type='text'
-                value={serverName}
-                onChange={(e) => setServerName(e.target.value)}
-            />
+        <div id='server-form-container'>
+            <form>
+                <h1 className="modal-title" id='serverFormTitle'>{title}</h1>
+                <label htmlFor='name' className='form-label'>Name{errors.name && <span className='formErrors'>: {errors.name}</span>}</label>
+                {/* {errors.name && <p className='new-server-form-error'> Name Error: {errors.name}</p>} */}
+                <input
+                    className='server-form-input'
+                    name='name'
+                    id='new-server-form-name'
+                    placeholder='Workspace name'
+                    type='text'
+                    value={serverName}
+                    onChange={(e) => setServerName(e.target.value)}
+                />
 
-            <label htmlFor='profile_pic' className='form-label'>Picture URL</label>
-            {errors.serverPic && <p className='new-server-form-error'>Picture URL Error: {errors.serverPic}</p>}
-            <input
-                name='profile_pic'
-                placeholder='example: https://funny-pic.png'
-                value={pfpURL}
-                onChange={(e) => setPfpURL(e.target.value)}
-            />
+                <label htmlFor='profile_pic' className='form-label'>Picture URL{errors.serverPic && <span className='formErrors'>: {errors.serverPic}</span>}</label>
+                {/* {errors.serverPic && <p className='new-server-form-error'>Picture URL Error: {errors.serverPic}</p>} */}
+                <input
+                    className='server-form-input'
+                    name='profile_pic'
+                    placeholder='Example: https://funny-pic.png'
+                    value={pfpURL}
+                    onChange={(e) => setPfpURL(e.target.value)}
+                />
 
-            <label htmlFor='description' className='form-label'>Description</label>
-            {errors.description && <p className='new-server-form-error'>Description Error: {errors.description}</p>}
-            <input
-                name='description'
-                placeholder='workspace description'
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-            />
+                <label htmlFor='description' className='server-form-label'>Description{errors.description && <span className='formErrors'>: {errors.description}</span>}</label>
+                {/* {errors.description && <p className='new-server-form-error'>Description Error: {errors.description}</p>} */}
+                <textarea
+                    contentEditable
+                    id='serverFormDescription'
+                    name='description'
+                    placeholder='Workspace description'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
 
-            {/* <div>
+                {/* <div>
         <label>Privacy</label>
         <div>
           <input
-            name='is-public'
-            type='radio'
-            id='public'
-            value="public"
-            onChange={() => setSelectedOption('public')}
-            checked={selectedOption === 'public'}
+          name='is-public'
+          type='radio'
+          id='public'
+          value="public"
+          onChange={() => setSelectedOption('public')}
+          checked={selectedOption === 'public'}
           />
           <label htmlFor='public'>Public</label>
+          </div>
+          {<div>
+            <input
+            name='is-public'
+            type='radio'
+            id='private'
+            value="private"
+            onChange={() => setSelectedOption('private')}
+            checked={selectedOption === 'private'}
+            />
+            <label htmlFor='private'>Private</label>
+            </div>}
+        </div> */}
+
+                <button className="form-button" type='submit' onClick={handleSubmit}>Submit</button>
+
+            </form>
         </div>
-        {<div>
-                    <input
-                        name='is-public'
-                        type='radio'
-                        id='private'
-                        value="private"
-                        onChange={() => setSelectedOption('private')}
-                        checked={selectedOption === 'private'}
-                    />
-                    <label htmlFor='private'>Private</label>
-                </div>}
-      </div> */}
-
-            <button className="form-button" type='submit' onClick={handleSubmit}>Submit</button>
-
-        </form>
     )
 }
 
