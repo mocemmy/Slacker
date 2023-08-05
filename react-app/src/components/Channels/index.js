60bf94ff87d73ee20ee957abfe72c728175cd4aa
Emily Morgan <mocemmy@aol.com>
2023-08-05 09:35:11 -0400

    fixed server dropdown and fixed channel name not changing on edit

--- react-app/src/components/Channels/index.js
+++ react-app/src/components/Channels/index.js
@@ -5,10 +5,9 @@ import { thunkGetAllMessages } from "../../store/messages";
 import Messages from "../Messages";
 import "./channelList.css";
 import OpenModalButton from "../OpenModalButton";
-import ConfirmModal from "../ConfirmModal";
-import { thunkLeaveChannel, thunkDeleteChannel } from "../../store/channels";
 import ChannelForm from "../ChannelForm";
 import ServerDropdown from "../ServerDropdown";
+import ChannelDropdown from "../ChannelDropdown";
 
 function Channels({ server }) {
     const channels = useSelector((state) => state.channels.channelList);
@@ -16,7 +15,7 @@ function Channels({ server }) {
     const [currChannel, setCurrChannel] = useState();
     const currentUser = useSelector((state) => state.session.user);
     
-    const [showChannelMenu, setShowChannelMenu] = useState(false);
+
     const ulRef = useRef();
     const channelRef = useRef();
 
@@ -30,6 +29,8 @@ function Channels({ server }) {
                 setCurrChannel(channels[0]);
                 dispatch(thunkGetAllMessages(channels[0].id));
             } else {
+                const newChannel = channels.find(channel => channel.id === currChannel.id)
+                setCurrChannel(newChannel)
                 dispatch(thunkGetAllMessages(currChannel.id))
             }
         }
@@ -37,18 +38,6 @@ function Channels({ server }) {
 
 
 
-    useEffect(() => {
-        if (!showChannelMenu) return;
-
-        const closeMenu = (e) => {
-            if (channelRef.current && !channelRef.current.contains(e.target))
-                setShowChannelMenu(false);
-        };
-
-        document.addEventListener("click", closeMenu); //close menu on click anywhere on document exept menu or button
-
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showChannelMenu]);
 
     if (!channels || !channels.length || !currChannel) return <EmptyChannels server={server} />;
     const changeChannel = (e, currChannel) => {
@@ -56,37 +45,6 @@ function Channels({ server }) {
         // setDefaultChannel(currChannel.id);
         setCurrChannel(currChannel);
     };
-
-    
-
-    const toggleChannelMenu = () => {
-        if (!showChannelMenu) setShowChannelMenu(true);
-        else setShowChannelMenu(false);
-    };
-
-    const ulChannelName = showChannelMenu ? "ul-dropdown" : "ul-dropdown hidden";
-
-    const ownedChannel =
-        currentUser && currChannel && currentUser.id === currChannel.created_by
-            ? "ul-channel"
-            : "hidden";
-    const notOwnedChannel =
-        currentUser && currChannel && currentUser.id !== currChannel.created_by
-            ? "ul-channel"
-            : "hidden";
-
-    const handleLeaveChannel = () => {
-        if (currentUser.id == 15 || currentUser.id == 16) {
-            window.alert("Demo users can't leave existing channels, Please create your own channel.")
-        } else {
-            dispatch(thunkLeaveChannel(currChannel.id, server.id))
-        }
-    }
-
-    const handleDeleteChannel = () => {
-        dispatch(thunkDeleteChannel(currChannel.id, server.id))
-    }
-
 
     return (
         <>
@@ -115,35 +73,7 @@ function Channels({ server }) {
                 </ul>
             </div>
             <div id="message-container">
-                <div
-                    className="channel-title"
-                    ref={channelRef}
-                    onClick={toggleChannelMenu}
-                >
-                    <p>{currChannel.name}&nbsp;<i class="fa-solid fa-arrow-down-short-wide"></i></p>
-                </div>
-                <div className={ulChannelName}>
-                    <ul className={ownedChannel}>
-                        <li>
-                            <OpenModalButton modalComponent={<ChannelForm type="UPDATE" channel={currChannel} server={server} />} buttonText="Edit Channel" />
-                        </li>
-                        <li>
-                            <OpenModalButton buttonText={'Delete Channel'} modalComponent={<ConfirmModal modalTitle={`Are you sure you want to delete the channel ${currChannel.name}?`} yesHandler={handleDeleteChannel} />} />
-                        </li>
-                    </ul>
-                    <ul className={notOwnedChannel}>
-                        <li>
-                            <OpenModalButton
-                                buttonText={"Leave Channel"}
-                                modalComponent={
-                                    <ConfirmModal
-                                        modalTitle={`Are you sure you want to leave ${currChannel.name}?`}
-                                        yesHandler={handleLeaveChannel}
-                                    />}
-                            />
-                        </li>
-                    </ul>
-                </div>
+                <ChannelDropdown currChannel={currChannel} server={server} />
                 <Messages channel={currChannel.id} />
             </div>
         </>
