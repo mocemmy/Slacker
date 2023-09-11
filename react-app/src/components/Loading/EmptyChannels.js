cf72526581a6e7ed61c2cc90f79cc022bef03a45
Emily Morgan <mocemmy@aol.com>
2023-09-11 14:50:09 -0400

    added search buttons to empty servers and channels

--- react-app/src/components/Loading/EmptyChannels.js
+++ react-app/src/components/Loading/EmptyChannels.js
@@ -2,26 +2,36 @@ import OpenModalButton from "../OpenModalButton";
 import ChannelForm from "../ChannelForm";
 import EmptyServers from "./EmptyServers";
 import ServerDropdown from "../ServerDropdown";
+import ChannelSearch from "../SearchComponents/ChannelSearch";
 
 function EmptyChannels({ server }) {
+  if (!server) return <EmptyServers />;
+  return (
+    <>
+      <div id="channel-container">
+        <ServerDropdown server={server} />
+        <ul id="channelList">
+          <h4 id="channelList-header">No Channels</h4>
 
-    if (!server) return <EmptyServers />
-    return (
-        <>
-            <div id="channel-container">
-                <ServerDropdown server={server} />
-                <ul id="channelList">
-                    <h4 id="channelList-header">No Channels</h4>
-
-                    <li className="channelListItem">
-                        <i className="fa-solid fa-plus"></i>&nbsp;&nbsp;
-                        <OpenModalButton buttonText="Add Channel" modalComponent={<ChannelForm type="CREATE" server={server} />} />
-                    </li>
-                </ul>
-            </div>
-            <div id="message-container"></div>
-        </>
-    );
+          <li className="channelListItem">
+            <i className="fa-solid fa-plus"></i>&nbsp;&nbsp;
+            <OpenModalButton
+              buttonText="Add Channel"
+              modalComponent={<ChannelForm type="CREATE" server={server} />}
+            />
+          </li>
+          <li className="channelListItem">
+            <i className="fa-solid fa-magnifying-glass"></i>
+            <OpenModalButton
+              buttonText="Search Channels"
+              modalComponent={<ChannelSearch serverId={server.id} />}
+            />
+          </li>
+        </ul>
+      </div>
+      <div id="message-container"></div>
+    </>
+  );
 }
 
 export default EmptyChannels;
--- react-app/src/components/Loading/EmptyServers.js
+++ react-app/src/components/Loading/EmptyServers.js
@@ -1,27 +1,38 @@
 import ServerListModalListItem from "../ServerList/ServerListModalListItem";
 import CreateServerForm from "../CreateServerForm";
 import ProfileDropdown from "../ProfileDropdown";
+import OpenModalButton from "../OpenModalButton";
+import WorkspaceSearch from "../SearchComponents/WorkspaceSearch";
 
 function EmptyServers({ setCurrChannel, setCurrServer }) {
-    return (
-        <>
-            <div id="serverList-container">
-                <ProfileDropdown setCurrChannel={setCurrChannel} setCurrServer={setCurrServer} />
-                <ul id="serverList">
-                    <ServerListModalListItem modalComponent={<CreateServerForm />} />
-                </ul>
-            </div>
-            <div id="channel-container">
-                <div id="serverNameContainer">
-                    <p id="serverName">No Workspaces Yet!</p>
-                </div>
-                <ul id="channelList">
-                    <h4 id="channelList-header">No Channels</h4>
-                </ul>
-            </div>
-            <div id="message-container"></div>
-        </>
-    );
+  return (
+    <>
+      <div id="serverList-container">
+        <ProfileDropdown
+          setCurrChannel={setCurrChannel}
+          setCurrServer={setCurrServer}
+        />
+        <ul id="serverList">
+          <li id="search-servers" className="serverListItem">
+            <OpenModalButton
+              buttonText={<i className="fa-solid fa-magnifying-glass"></i>}
+              modalComponent={<WorkspaceSearch />}
+            />
+          </li>
+          <ServerListModalListItem modalComponent={<CreateServerForm />} />
+        </ul>
+      </div>
+      <div id="channel-container">
+        <div id="serverNameContainer">
+          <p id="serverName">No Workspaces Yet!</p>
+        </div>
+        <ul id="channelList">
+          <h4 id="channelList-header">No Channels</h4>
+        </ul>
+      </div>
+      <div id="message-container"></div>
+    </>
+  );
 }
 
-export default EmptyServers
+export default EmptyServers;
